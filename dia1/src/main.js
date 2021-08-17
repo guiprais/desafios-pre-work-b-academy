80c7b67366468a570824fbfb19047edd3c35eb5b
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-17 10:22:10 -0300

    refactor: texto mostrar/esconder dinâmico

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -9,13 +9,16 @@ app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+button.innerHTML = "Clique aqui para exibir as boas-vindas";
 
 button.addEventListener("click", (event) => {
   event.preventDefault;
 
   if (app.style.display === "none") {
     app.style.display = "block";
+    button.innerHTML = "Clique aqui para esconder as boas-vindas";
   } else {
     app.style.display = "none";
+    button.innerHTML = "Clique aqui para exibir as boas-vindas";
   }
 });

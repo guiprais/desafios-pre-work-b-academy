2f195ab3c7bde5923bbf90bd71ee54397fba5a2a
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-17 10:04:39 -0300

    refactor: arrow function e <a> no lugar de button

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,21 +1,21 @@
-import './style.css';
+import "./style.css";
 
 const app = document.querySelector('[data-js="app"]');
+const button = document.querySelector('[data-js="button"]');
 
-app.style.display = 'none';
+app.style.display = "none";
 
 app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
 
-const button = document.querySelector('[data-js="button"]');
-function showApp() {
-  if (app.style.display === 'none') {
-    app.style.display = 'block';
-  } else {
-    app.style.display = 'none';
-  }
-}
+button.addEventListener("click", (event) => {
+  event.preventDefault;
 
-button.addEventListener('click', showApp);
+  if (app.style.display === "none") {
+    app.style.display = "block";
+  } else {
+    app.style.display = "none";
+  }
+});

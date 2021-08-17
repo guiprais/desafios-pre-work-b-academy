a2bdaa829c3e91dbbaf10ec9e7d641693e85f2da
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-17 09:45:45 -0300

    desafio 4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,21 @@
-import './style.css'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
+const app = document.querySelector('#app');
+
+app.style.display = 'none';
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
-`
+`;
+
+const button = document.querySelector('#button');
+function showApp() {
+  if (app.style.display === 'none') {
+    app.style.display = 'block';
+  } else {
+    app.style.display = 'none';
+  }
+}
+
+button.addEventListener('click', showApp);

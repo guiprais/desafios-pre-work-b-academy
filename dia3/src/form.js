e5a89e72e6ddf9b38b70163cd7f410321c4fc6bb
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-19 13:45:44 -0300

    desafio 02

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,3 +1,4 @@
+// Desafio 1
 const formatName = (string) =>
   string
     .toLowerCase()
@@ -5,9 +6,68 @@ const formatName = (string) =>
 
 const nameInput = document.querySelector('[data-js="name"]');
 
-nameInput.addEventListener("input", ({ target }) => {
-  const inputFormated = formatName(target.value);
-  nameInput.value = inputFormated;
+const formatInput = () => {
+  nameInput.addEventListener("input", ({ target }) => {
+    const inputFormated = formatName(target.value);
+    nameInput.value = inputFormated;
+  });
+};
+
+formatInput();
+
+// Desafio 2
+const cores = ["red", "blue", "pink", "purple", "lightgreen"];
+
+const createColorsOption = () => {
+  const form = document.querySelector('[data-js="form"]');
+  const select = document.createElement("select");
+  select.setAttribute("multiple", "");
+
+  cores.forEach((cor) => {
+    const option = document.createElement("option");
+    option.innerText = cor;
+    select.appendChild(option);
+  });
+
+  form.appendChild(select);
+};
+createColorsOption();
+
+const createSquareContainer = () => {
+  const squareContainer = document.createElement("div");
+  squareContainer.setAttribute("data-js", "square-container");
+  document.body.appendChild(squareContainer);
+};
+createSquareContainer();
+
+const colorsOption = document.querySelectorAll("option");
+
+colorsOption.forEach((color) => {
+  color.addEventListener("click", ({ target }) => {
+    const squares = document.querySelectorAll(
+      '[data-js="square-container"] div'
+    );
+
+    const arrayOfSquares = [...squares];
+    const findSquare = arrayOfSquares.find(
+      (square) => square.style.backgroundColor === target.value
+    );
+
+    if (findSquare) {
+      const container = document.querySelector('[data-js="square-container"]');
+      const child = document.querySelector(`.${target.value}`);
+      container.removeChild(child);
+    } else {
+      createSquare(target.value);
+    }
+  });
 });
 
-formatName("Guilherme de prais");
+const createSquare = (color) => {
+  const container = document.querySelector('[data-js="square-container"]');
+  const square = document.createElement("div");
+  square.className = `quadrado ${color}`;
+  square.style.backgroundColor = `${color}`;
+
+  container.appendChild(square);
+};

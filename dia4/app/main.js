63f79a4a530d1cfd72b6320bf4798e3752f10abb
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-20 12:33:17 -0300

    desafio 03, catch não funcionando

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -2,20 +2,68 @@ import "./style.css";
 
 const carsForm = document.querySelector('[data-js="submit-car-form"]');
 const tableBody = document.querySelector('[data-js="table"]');
+const url = "http://localhost:3333/cars";
 
 async function request(url) {
   const result = await fetch(url);
   return await result.json();
 }
 
-request("http://localhost:3333/cars").then((response) => {
-  if (response.length === 0) {
-    console.log(response);
+function showCars() {
+  request(url).then((response) => {
     const tr = document.createElement("tr");
-    const td = document.createElement("td");
-    td.textContent = "Nenhum carro encontrado";
-    tr.appendChild(td);
 
-    tableBody.appendChild(tr);
-  }
+    if (response.length === 0) {
+      const td = document.createElement("td");
+      td.textContent = "Nenhum carro encontrado";
+      tr.appendChild(td);
+
+      tableBody.appendChild(tr);
+    } else {
+      tableBody.innerHTML = "";
+      response.forEach((car) => {
+        const tr = document.createElement("tr");
+        const values = Object.values(car);
+        values.forEach((value) => {
+          const td = document.createElement("td");
+          td.textContent = value;
+          tr.appendChild(td);
+        });
+        tableBody.appendChild(tr);
+      });
+    }
+  });
+}
+
+const getFormElement = (event) => (elementName) => {
+  return event.target.elements[elementName];
+};
+
+carsForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const getElement = getFormElement(event);
+  const image = getElement("image").value;
+  const brandModel = getElement("model").value;
+  const year = getElement("year").value;
+  const plate = getElement("plate").value;
+  const color = getElement("color").value;
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      image,
+      brandModel,
+      year,
+      plate,
+      color,
+    }),
+  })
+    .then((result) => result.ok && showCars())
+    .catch(() => "Carro já cadastrado");
 });
+
+window.onload = showCars;

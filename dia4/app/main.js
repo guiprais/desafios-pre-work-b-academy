b96dd8177c8ed6498ee0cb483f3fd48fc1d152ef
Guilherme de Prais <guilherme.prais@gmail.com>
2021-08-20 11:32:52 -0300

    desafio 01 e 02

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,1 +1,21 @@
-import './style.css'
+import "./style.css";
+
+const carsForm = document.querySelector('[data-js="submit-car-form"]');
+const tableBody = document.querySelector('[data-js="table"]');
+
+async function request(url) {
+  const result = await fetch(url);
+  return await result.json();
+}
+
+request("http://localhost:3333/cars").then((response) => {
+  if (response.length === 0) {
+    console.log(response);
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    td.textContent = "Nenhum carro encontrado";
+    tr.appendChild(td);
+
+    tableBody.appendChild(tr);
+  }
+});
